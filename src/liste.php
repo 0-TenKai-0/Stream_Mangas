0ead5360b3064f176dff66aaa185fa924877ab92
0-Tenkai-0 <nico.collignon0@gmail.com>
2021-01-27 22:00:12 +0100

    Add files 2

--- src/liste.php
+++ src/liste.php
@@ -16,37 +16,38 @@ $objPdo->query('SET NAMES utf8');
 </head>
 
 <body>
-<form method="post" action = "liste.php">
-    <header>
-        <img src="images/Logo.png" alt="" width="90" height="90" />
+    <form method="post" action = "liste.php">
+        <header>
+            <img src="images/Logo.png" alt="" width="90" height="90" />
 
-        <h1 class="titre_site" ><a href="accueil.php">MANGAS</a></h1>
-        <div class="menu">
-            <a href="liste.php">
-                <article class="left">
-                    LISTE
-                </article>
-            </a>
-        </div>
-        <div class="menu">
-            <a href="">
-                <article class="middle">
-                    NEWS
-                </article>
-            </a>
-        </div>
-        <div class="menu">
-            <a href="">
-                <article class="right">
-                    NEWS
-                </article>
-            </a>
-        </div>
-    </header>
-    <div>
-        <article class="fond">
+            <h1 class="titre_site" ><a href="accueil.php">MANGAS</a></h1>
+            <div class="menu">
+                <a href="liste.php">
+                    <article class="left">
+                        LISTE
+                    </article>
+                </a>
+            </div>
+            <div class="menu">
+                <a href="">
+                    <article class="middle">
+                        NEWS
+                    </article>
+                </a>
+            </div>
+            <div class="menu">
+                <a href="">
+                    <article class="right">
+                        NEWS
+                    </article>
+                </a>
+            </div>
+        </header>
+        <div>
+            <article class="fond">
 
-        </article>
-    </div>
+            </article>
+        </div>
+    </form>
 </body>
 </html>
