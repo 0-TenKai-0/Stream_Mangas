e51fbdfb143a6ac126a0224e16a79bb48c5c5890
0-Tenkai-0 <nico.collignon0@gmail.com>
2021-01-27 21:25:53 +0100

    Add files

---
+++ src/accueil.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="FR">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" media="screen and (min-width:0px)" href="style_computer.css">
+    <link rel="icon" type="images/x-icon" href="images/Logo.png" />
+</head>
+
+<body>
+    <from method="post" action = "accueil.php">
+        <header>
+            <img src="images/Logo.png" alt="" width="90" height="90" />
+            <title>Stream Mangas</title>
+            <h1 class="titre_site" href="accueil.php">STREAM MANGAS</h1>
+            <div class="menu">
+                <a href="liste.php">
+                    <article class="left">
+                        LISTE
+                    </article>
+                </a>
+            </div>
+            <div class="menu">
+                <a href="">
+                    <article class="middle">
+                        NEWS
+                    </article>
+                </a>
+            </div>
+            <div class="menu">
+                <a href="">
+                    <article class="right">
+                        NEWS
+                    </article>
+                </a>
+            </div>
+        </header>
+</body>
